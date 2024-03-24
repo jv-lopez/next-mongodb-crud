269d0bb8a1abb30faf84adb616e82086d3689cee
me-jameshwartlopez <me@jameshwartlopez.com>
2024-03-24 21:24:53 +0800

    crud app using mongodb base on https://github.com/jv-lopez/next-supabase-crud

---
+++ app/page.tsx
@@ -0,0 +1,34 @@
+import prisma from '@/app/db'
+import { TodoList } from '@/app/components/TodoList'
+import { CreateTodo } from '@/app/components/forms/CreateTodo'
+
+async function getData() {
+  const data = await prisma.todo.findMany({
+    select: {
+      input: true,
+      id: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return data
+}
+
+export default async function Home() {
+  const data = await getData()
+  return (
+    <div className='h-screen w-screen flex flex-col gap-8 items-center justify-center'>
+      <h1 className='text-center text-4xl font-bold'>
+        CRUD APP - WITH MONGODB
+      </h1>
+      <div className='border rounded-lg shadow-xl p-10 w-1/3'>
+        <CreateTodo />
+        <div className='mt-5 flex flex-col gap-y-2'>
+          <TodoList data={data} />
+        </div>
+      </div>
+    </div>
+  )
+}
